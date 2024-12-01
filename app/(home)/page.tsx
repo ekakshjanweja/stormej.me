be4111586a04a4b7d77eecb4b4f5eb39bb16c7da
Ekaksh Janweja <jekaksh@gmail.com>
2024-12-02 00:39:24 +0530

    remove spotify

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,8 @@ import SpotifyCurrentPlayingBadge from "@/components/spotify-currently-playing-b
 import { BlogRow } from "./_components/blog-row";
 import Hero from "./_components/hero";
 import Section, { SectionType } from "@/components/section";
-import { getSpotifyCurrentPlaying } from "@/lib/spotify";
 
 export default async function Home() {
-  const currentPlayingInfo = await getSpotifyCurrentPlaying();
-
   return (
     <div>
       <Hero />
@@ -14,12 +11,6 @@ export default async function Home() {
       <Section sectionType={SectionType.work} />
       <Section sectionType={SectionType.project} />
       <BlogRow />
-      <div className="mt-8">
-        <SpotifyCurrentPlayingBadge
-          title="Listening to"
-          spotifyInfo={currentPlayingInfo}
-        />
-      </div>
     </div>
   );
 }
