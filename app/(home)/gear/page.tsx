505e5a3e41102cf25fb11741bc1f6d475bb7273b
Ekaksh Janweja <jekaksh@gmail.com>
2025-05-01 20:41:59 +0530

    misc

--- app/(home)/gear/page.tsx
+++ app/(home)/gear/page.tsx
@@ -1,15 +1,17 @@
 import HeadlineLarge from "@/components/styles/headline-large";
+import HeadlineSmall from "@/components/styles/headline-small";
 
 const gear = [
-  { label: "typing 30s", value: "86wpm" },
+  { label: "laptop", value: "m4 macbook pro base/g14'21" },
   { label: "keyboard", value: "aula f75 pro" },
   { label: "mouse", value: "logitech g304" },
-  { label: "pc", value: "7600-4060-32gbddr5" },
+  { label: "pc", value: "7600-4060-32gb_ddr5" },
   { label: "monitor", value: "gigabyte g24f2" },
+  { label: "headphones", value: "soundcore q20i" },
   { label: "mic", value: "fifine at6" },
   { label: "webcam", value: "lenovo 300" },
   { label: "mobile", value: "pixel 6a" },
-  { label: "laptop", value: "zephyrus g14 2021" },
+  { label: "typing 30s", value: "86wpm" },
 ];
 
 export default function Gear() {
@@ -24,10 +26,12 @@ export default function Gear() {
               <div key={label} className="text-left">
                 {label}
               </div>
-              <div className="text-right">{value}</div>
+              <div className="text-left">{value}</div>
             </>
           ))}
         </div>
+
+        <HeadlineSmall text="idk whats left" className="mt-4" />
       </div>
     </>
   );
